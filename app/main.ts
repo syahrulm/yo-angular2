600c6b690dfb02c6ec7f33ea143edd3fd9e79922
syahrulm <syahrulmunif@live.jp>
2016-02-15 15:29:09 +0700

    Init commit

---
+++ app/main.ts
@@ -0,0 +1,8 @@
+import "zone.js/dist/zone.min.js";
+import "reflect-metadata";
+
+import {bootstrap} from "angular2/platform/browser";
+import {provide} from "angular2/core";
+import {AppComponent} from "./app.component";
+
+bootstrap(AppComponent);
